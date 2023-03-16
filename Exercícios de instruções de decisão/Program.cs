b67b2d4e7588cbf94499da93f3c4cd3c9914608e
ardoleon <ardoleon@LAPTOP777L>
2023-03-15 22:31:42 -0400

    Adicionar arquivos de projeto.

---
+++ Exercícios de instruções de decisão/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercícios_de_instruções_de_decisão
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
