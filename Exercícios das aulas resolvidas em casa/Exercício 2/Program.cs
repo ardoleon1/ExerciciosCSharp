14a70cf0e6f31d040e233b93b47902df86d25896
Leonardo Pereira Mendes Ribas <loen.ribas20@gmail.com>
2023-05-11 10:23:24 -0400

    Exercício 2 feito

---
+++ Exercícios das aulas resolvidas em casa/Exercício 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---Exercício 2---");
+            int numeroTotal = 0;
+            for (int i = 49; i <= 149; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                    numeroTotal += i;
+                }
+            }
+            Console.WriteLine("A soma total é " + numeroTotal);
+            Console.ReadLine();
+        }
+    }
+}
