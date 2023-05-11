450095c8aada5951cb02eaf42d359355ecff4d1a
Leonardo Pereira Mendes Ribas <loen.ribas20@gmail.com>
2023-05-11 10:21:15 -0400

    Exercício 1 feito

---
+++ Exercícios das aulas resolvidas em casa/Exercícios sobre números impares e pares/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercícios_sobre_números_impares_e_pares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---Exercício 1---");
+            int numeroTotal = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                    numeroTotal += i;
+                }
+            }
+            Console.WriteLine("A soma total é " + numeroTotal);
+            Console.ReadLine();
+        }
+    }
+}
