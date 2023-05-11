09df45c125ab501e882f67a336b4a4ca070f8a89
Leonardo Pereira Mendes Ribas <loen.ribas20@gmail.com>
2023-05-11 10:04:19 -0400

    Teste arrumado

--- teste/Program.cs
+++ teste/Program.cs
@@ -12,15 +12,7 @@ namespace teste
         {
             Console.WriteLine("\t\t---Teste---");
 
-            /* 4 Conceitos essenciais: Variáveis, Condicionais, Laços de repetição e Coleções
-             * 
-             * 1.Quais são os dados necessários?
-             * 2.O que fazer com esses dados?
-             * 3.O que o programa pede essencialmente?
-             * 4.Qual é o resultado esperado?
-             * 5.Qual é sequência de passos a ser feita para atingir o resultado?
-             * 
-            */
+
 
             Console.ReadLine();
         }
